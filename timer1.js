b35660e14e56a2a7f3877e7f69ab85a8a10b5061
Sofia Panchaud <s.panchaud.89@gmail.com>
2023-11-07 17:48:25 -0500

    only beep when input is a number greater than or equal to 0

--- timer1.js
+++ timer1.js
@@ -2,9 +2,8 @@ const userAlarms = process.argv.slice(2);
 
 for (const alarm of userAlarms) {
   setTimeout(() => {
-    console.log(alarm);
+    if (alarm >= 0) {
+      process.stdout.write('\x07');
+    }
   }, alarm * 1000);
 }
-
-
-// process.stdout.write('\x07');
