af58420dc5ad9f38841baeb76db4cce7d8fd474e
Sofia Panchaud <s.panchaud.89@gmail.com>
2023-11-09 12:34:20 -0500

    add comments

--- timer2.js
+++ timer2.js
@@ -3,20 +3,27 @@ const stdin = process.stdin;
 stdin.setRawMode(true);
 stdin.setEncoding('utf8');
 
+// on input
 stdin.on('data', (key) => {
+  // if user pressed b, immediately beep
   if (key === "b") {
     process.stdout.write('\x07');
+    // exit on ctrl + c
   } else if (key === '\u0003') {
     process.exit();
   }
+  // if key is a number greater than 0
   if (key > 0) {
+    // print message to console
     console.log(`setting alarm for ${key} seconds`);
+    // beep after specified number of seconds
     setTimeout(() => {
       process.stdout.write('\x07');
     }, key * 1000);
   }
 });
 
+// on exit, print message to console
 process.on('exit', () => {
   console.log("Thanks for using me, ciao!");
 });
