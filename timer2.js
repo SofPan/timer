20162ad8fe139a6388bc557f0b1df19a5c1a3dde
Sofia Panchaud <s.panchaud.89@gmail.com>
2023-11-09 11:53:36 -0500

    implement ctrl + c to close readline

--- timer2.js
+++ timer2.js
@@ -1,13 +1,24 @@
-// capture any number of user input arguments
-const userAlarms = process.argv.slice(2);
-
 // iterate over user input array
-for (const alarm of userAlarms) {
-  setTimeout(() => {
-    // if the alarm is a number greater than or equal to zero, cause a beep
-    if (alarm >= 0) {
-      process.stdout.write('\x07');
-    }
-    // beep should occur at the intervals set by the user
-  }, alarm * 1000);
-}
+// for (const alarm of userAlarms) {
+//   setTimeout(() => {
+//     // if the alarm is a number greater than or equal to zero, cause a beep
+//     if (alarm >= 0) {
+//       process.stdout.write('\x07');
+//     }
+//     // beep should occur at the intervals set by the user
+//   }, alarm * 1000);
+// }
+
+const readline = require('readline');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+rl.prompt(input => {
+  if (input === '\u0003') {
+
+    rl.close();
+  }
+});
